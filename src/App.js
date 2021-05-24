023559324ed7775c703f9c6e3562dc9eb2a07255
PauloDev <devpgm2020@gmail.com>
2021-05-24 15:38:05 -0300

    Dividindo ExpenseItem em vários componentes

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ function App() {
       id: 1,
       title: 'Renault Clio',
       amount: '21.000,00',
-      date: new Date(2010, 1, 1),
+      date: new Date(2010, 2, 1),
     },
     {
       id: 2,
       title: 'Renault Sandero',
       amount: '34.000,00',
-      date: new Date(2020, 1, 1),
+      date: new Date(2020, 3, 1),
     },
     {
       id: 3,
       title: 'Ford KA',
       amount: '31.000,00',
-      date: new Date(2018, 1, 1),
+      date: new Date(2018, 4, 1),
     },
     {
       id: 4,
       title: 'Wolkswagem Gol',
       amount: '28.000,00',
-      date: new Date(2015, 1),
+      date: new Date(2015, 5, 1),
     },
   ];
   return (
