85dd35960fa48943a2d75a43253f8177018f664a
PauloDev <devpgm2020@gmail.com>
2021-05-24 14:31:56 -0300

    Criando e adicionando primeiro componente ao projeto

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>24 de maio de 2021</div>
+      <div>
+        <h2>Carrro</h2>
+        <div>R$ 21.000,00</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
