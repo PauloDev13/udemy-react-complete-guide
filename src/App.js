52166f541d52097653160f95e72bf779b9454730
PauloDev <devpgm2020@gmail.com>
2021-05-31 22:03:56 -0300

    Passando eventos através de props de componente filho para componente pai

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
 import Expenses from './components/expenses/Expenses';
 import NewExpense from './components/newExpense/NewExpense';
-import ExpenseState from './context/ExpenseState';
+// import ExpenseState from './context/ExpenseState';
 
 const App = () => {
-  // const expenses = [
-  //   {
-  //     id: 1,
-  //     title: 'Renault Clio',
-  //     amount: '21.000,00',
-  //     date: new Date(2010, 2, 1),
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Renault Sandero',
-  //     amount: '34.000,00',
-  //     date: new Date(2020, 3, 1),
-  //   },
-  //   {
-  //     id: 3,
-  //     title: 'Ford KA',
-  //     amount: '31.000,00',
-  //     date: new Date(2018, 4, 1),
-  //   },
-  //   {
-  //     id: 4,
-  //     title: 'Wolkswagem Gol',
-  //     amount: '28.000,00',
-  //     date: new Date(2015, 5, 1),
-  //   },
-  // ];
+  const expenses = [
+    {
+      id: 1,
+      title: 'Renault Clio',
+      amount: '21.000,00',
+      date: new Date(2010, 2, 1),
+    },
+    {
+      id: 2,
+      title: 'Renault Sandero',
+      amount: '34.000,00',
+      date: new Date(2020, 3, 1),
+    },
+    {
+      id: 3,
+      title: 'Ford KA',
+      amount: '31.000,00',
+      date: new Date(2018, 4, 1),
+    },
+    {
+      id: 4,
+      title: 'Wolkswagem Gol',
+      amount: '28.000,00',
+      date: new Date(2015, 5, 1),
+    },
+  ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('No App.js', expense);
+  };
+
   return (
     <div>
-      <ExpenseState>
-        <NewExpense />
-        {/* <Expenses items={expenses} /> */}
-        <Expenses />
-      </ExpenseState>
+      {/* <ExpenseState> */}
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+      {/* <Expenses /> */}
+      {/* </ExpenseState> */}
     </div>
   );
 };
--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,24 +1,24 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import './Expenses.css';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import expenseContext from '../../context/expense-context';
+// import expenseContext from '../../context/expense-context';
 
 const Expenses = (props) => {
-  const { expenses } = useContext(expenseContext);
+  // const { expenses } = useContext(expenseContext);
 
   return (
     <Card className="expenses">
-      {expenses.map((expense) => (
+      {/* {expenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
         />
-      ))}
-      {/* <ExpenseItem
+      ))} */}
+      <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -37,7 +37,7 @@ const Expenses = (props) => {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      /> */}
+      />
     </Card>
   );
 };
